67a38eddfd5e1b738ac99811ef5d885f57d4c5e1
Maxwell Viana <maxwell.viana@elogroup.com.br>
2024-01-30 10:21:02 -0300

    :build: < Configuração de relatórios mocha preparação ambiente >

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,20 @@
 const { defineConfig } = require("cypress");
+const moment = require('moment');
+
+const dateTime = moment().format('YYYY-MM-DD_HH-mm-ss');
 
 module.exports = defineConfig({
-  chromeWebSecurity: false,
   e2e: {
-    baseUrl: '',
+    baseUrl: 'https://example.cypress.io',
     video: true,
   },
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: `cypress/reports/${dateTime}`,
+    quiet: true,
+    overwrite: false,
+    html: true,
+    json: true,
+  },
+  screenshotsFolder: `cypress/reports/${dateTime}/evidências`,
 });
